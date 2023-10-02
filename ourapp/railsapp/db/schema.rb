efde1539e96eb277e3845684e1c190ac8d03fdf4
Alex <akp42@duke.edu>
2023-10-01 22:40:23 -0400

    added q

--- ourapp/railsapp/db/schema.rb
+++ ourapp/railsapp/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_015937) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_02_023353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "question"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "test_users", force: :cascade do |t|
     t.string "name"
--- ourapp/railsapp/db/seeds.rb
+++ ourapp/railsapp/db/seeds.rb
@@ -6,10 +6,31 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
     #clear existing users
-    #User.destroy_all
+    TestUser.destroy_all
+    Question.destroy_all
     # user1 = User.create(unique_id: "aa111", first_name: "Firstname", last_name: "Lastname", email: "a@a.com", box_no: "99999", affiliation: "student", display_name: "Aaron Jackson")
     # user2 = User.create(unique_id: "bb222", first_name: "bbbbb", last_name: "ccccc", email: "b@b.com", box_no: "88888", affiliation: "admin", display_name: "Ria Cai")
+# *Create sample users
+user1 = TestUser.create(name: "Jacob", birthday: "060303")
+user2 = TestUser.create(name: "Linda", birthday: "031404")
+user3 = TestUser.create(name: "Eileen", birthday: "081504")
+user4 = TestUser.create(name: "Anna", birthday: "102903")
 
+
+# *Create sample questions
+
+question1 = Question.create(question: "I believe opposites attract.", category: "1")
+
+# t.string "name"
+# t.string "join_date"
+# t.string "location"
+# t.string "bio"
+# t.string "gender"
+# t.string "preferences"
+# t.string "birthday"
+# t.string "password"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
 
 #Timeslot.destroy_alls
 #     # Set time zone
