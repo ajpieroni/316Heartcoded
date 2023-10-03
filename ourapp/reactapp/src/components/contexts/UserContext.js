a274e325177ccf28f56bc7de2bbd8a2dc0f9b56f
Alex <akp42@duke.edu>
2023-10-03 00:55:40 -0400

    react setup

--- ourapp/reactapp/src/components/contexts/UserContext.js
+++ ourapp/reactapp/src/components/contexts/UserContext.js
@@ -1,7 +1,9 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 // import axios from "axios";
+import { useContext } from "react";
 
-// export const UserContext = React.createContext();
+export const UserContext = React.createContext();
+// export UserContext;
 
 // export const UserProvider = ({ children }) => {
 //   const [isApiCallComplete, setIsApiCallComplete] = useState(false);
