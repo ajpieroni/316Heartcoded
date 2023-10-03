2a0f0c16696e1c5c802371dc59cd087b2dc80b3c
Alex <akp42@duke.edu>
2023-10-02 21:24:54 -0400

    added scaffold and links

---
+++ ourapp/reactapp/src/pages/Questions.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+// import "./UserLanding.css";
+import axios from "axios";
+
+export default function Questions() {
+  const [data, setData] = useState();
+
+//   const fetchData = () => {
+//     fetch(`http://localhost:3000/questions/1`)
+//       .then((response) => response.json())
+//       .then((data) => setQuestion(data.question))
+//       .catch((error) => {
+//         console.error("Error fetching the question:", error);
+//       });
+//   };
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+  return (
+    <main className="main-container">
+        <h1>Questions</h1>
+ 
+    </main>
+  );
+}
