7d17b97a8ee3edeb9319da24af3c0b1033c48f43
Alex <akp42@duke.edu>
2023-10-03 12:35:07 -0400

    stablilize main:

--- ourapp/reactapp/src/pages/FindMatch.js
+++ ourapp/reactapp/src/pages/FindMatch.js
@@ -3,12 +3,17 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function FindMatch() {
-  const [data, setData] = useState();
+  const [matches, setMatches] = useState([]);
+  const currentUser = 53;
 
   const fetchData = () => {
-    fetch(`http://localhost:3000/matched_withs/1`)
+    fetch(`http://localhost:3000/matched_withs/users/${currentUser}`)
       .then((response) => response.json())
-      .then((data) => setData(data.date))
+      .then((data) => {
+        setMatches(data);
+        console.log("here is data", data);
+      }
+      )
       .catch((error) => {
         console.error("Error fetching the question:", error);
       });
@@ -20,8 +25,14 @@ export default function FindMatch() {
 
   return (
     <main className="main-container">
-        <h1>FindMatch</h1>
-        <h1>{data}</h1>
+        <h1>Current Matches</h1>
+        <ul>
+          {matches.map(match => (
+            <li key={match.id}>
+              UID1: {match.uid1}, UID2: {match.uid2}, Status: {String(match.status)}, Date: {match.date}
+            </li>
+          ))}
+        </ul>
     </main>
   );
 }
