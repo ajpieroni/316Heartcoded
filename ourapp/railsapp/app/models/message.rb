8cbb58d92acd40a13b615502e932ef1b2ca23c70
Alex <akp42@duke.edu>
2023-10-04 13:13:34 -0400

    Revert "stabilized main, lost chat and matches"
    This reverts commit a0c838095efb49855a278e7712117554856e450a.

--- ourapp/railsapp/app/models/message.rb
+++
@@ -1,5 +0,0 @@
-class Message < ApplicationRecord
-  belongs_to :uid_sender, class_name: 'TestUser', foreign_key: 'uid_sender_id'
-  belongs_to :uid_receiver, class_name: 'TestUser', foreign_key: 'uid_receiver_id'
-  
-end
