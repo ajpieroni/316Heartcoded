1a487cf1a42722ce20b11874c38a0f669540acda
Alex <akp42@duke.edu>
2023-10-04 18:43:22 -0400

    updated changes to dropped migrations, rollback and dropped stranded tables

--- ourapp/railsapp/db/schema.rb
+++ ourapp/railsapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_050832) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_223825) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_050832) do
     t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "chat_order"
+    t.bigint "uid_sender_id", null: false
+    t.bigint "uid_receiver_id", null: false
+    t.datetime "timestamp"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["uid_receiver_id"], name: "index_messages_on_uid_receiver_id"
+    t.index ["uid_sender_id"], name: "index_messages_on_uid_sender_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -54,4 +66,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_050832) do
 
   add_foreign_key "matched_withs", "test_users", column: "uid1"
   add_foreign_key "matched_withs", "test_users", column: "uid2"
+  add_foreign_key "messages", "test_users", column: "uid_receiver_id"
+  add_foreign_key "messages", "test_users", column: "uid_sender_id"
 end
--- ourapp/railsapp/db/seeds.rb
+++ ourapp/railsapp/db/seeds.rb
@@ -49,8 +49,11 @@ match10 = MatchedWith.create(uid1: 2, uid2: 6, status: true, date: "11-11-2000")
 
 
 # Create sample messages
-message1 = Message.create(chat_order: 1, uid_sender: user1, uid_receiver: user2, timestamp: Time.now, message: "Hey Linda, how's it going?")
-message2 = Message.create(chat_order: 2, uid_sender: user2, uid_receiver: user1, timestamp: Time.now + 5.minutes, message: "Hey Jacob! I'm good. Just building things.")
+message1 = Message.create(chat_order: 1, uid_sender_id: user1.id, uid_receiver_id: user2.id, timestamp: Time.now, message: "Hey Linda, how's it going?")
+message1 = Message.create(chat_order: 1, uid_sender_id: user2.id, uid_receiver_id: user1.id, timestamp: Time.now + 5.minutes, message: "Hey Jacob, going well!")
+
+# message1 = Message.create(chat_order: 1, uid_sender: user1, uid_receiver: user2, timestamp: Time.now, message: "Hey Linda, how's it going?")
+# message2 = Message.create(chat_order: 2, uid_sender: user2, uid_receiver: user1, timestamp: Time.now + 5.minutes, message: "Hey Jacob! I'm good. Just building things.")
 # message3 = Message.create(chat_order: , uid_sender: user1, uid_receiver: user2, timestamp: Time.now, message: "Hey Linda, how's it going?")
 # message4 = Message.create(chat_order: 2, uid_sender: user2, uid_receiver: user1, timestamp: Time.now + 5.minutes, message: "Hey Jacob! I'm good. Just building things.")
 
