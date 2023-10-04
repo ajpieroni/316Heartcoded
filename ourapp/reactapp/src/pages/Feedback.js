aa715c2ed0d572924768dca07bd15f159e2098f4
Jacob Lee <53345177+Jacob-W-Lee@users.noreply.github.com>
2023-10-04 01:26:35 -0400

    feedback form implemented badly

--- ourapp/reactapp/src/pages/Feedback.js
+++ ourapp/reactapp/src/pages/Feedback.js
@@ -6,12 +6,19 @@ import axios from "axios";
 
 export default function Feedback() {
   const [feedback, setFeedback] = useState();
-
+  const [receiver, setReceiver] = useState();
+  const [sender, setSender] = useState();
+  const [category, setCategory] = useState();
 
   const fetchData = () => {
-    fetch(`http://localhost:3000/feedbacks/1`)
+    fetch(`https://localhost:3000/feedbacks/1`)
       .then((response) => response.json())
-      .then((data) => setFeedback(data.feedback))
+      .then((data) => {
+        setFeedback(data.feedback);
+        setReceiver(data.receives_uid);
+        setSender(data.gives_uid);
+        setCategory(data.category);
+      })
       .catch((error) => {
         console.error("Error fetching the feedback:", error);
       });
@@ -23,10 +30,29 @@ export default function Feedback() {
     console.log(feedback);
   }, []);
 
+
   return (
     <main className="main-container">
         <h1>Feedback</h1>
-        <h1>What is your feedback about {feedback}</h1>
+        <h1>What is your feedback about {receiver}?</h1>
+
+        <h1> {feedback}</h1>
+
+        <h1>User Feedback Form</h1>
+        <p> Hello {sender}, provide feedback about a specific user: {receiver} in category: {category}</p>
+
+        <form action="submit_feedback.php" method="post">
+            <label htmlFor="user_to_feedback">User to Provide Feedback About: {receiver}</label>
+            <br></br>
+
+            <label htmlFor="feedback">Feedback:</label>
+            <br></br>
+
+            <textarea id="feedback" name="feedback" rows="4" cols="50" required></textarea>
+            <br></br>
+
+            <input type="submit" value="Submit Feedback"/>
+        </form>
     </main>
   );
 }
