6d1370a8e8d272109ef60c1ae63e058b3422ecc7
Alex <akp42@duke.edu>
2023-10-04 22:37:06 -0400

    name fetching

--- ourapp/reactapp/src/pages/Feedback.js
+++ ourapp/reactapp/src/pages/Feedback.js
@@ -9,50 +9,92 @@ export default function Feedback() {
   const [receiver, setReceiver] = useState();
   const [sender, setSender] = useState();
   const [category, setCategory] = useState();
+  const { user, setUser } = useContext(UserContext);
 
-  const fetchData = () => {
-    fetch(`http://localhost:3000/feedbacks/1`)
-      .then((response) => response.json())
-      .then((data) => {
-        setFeedback(data.feedback);
-        setReceiver(data.receives_uid);
-        setSender(data.gives_uid);
-        setCategory(data.category);
-      })
-      .catch((error) => {
-        console.error("Error fetching the feedback:", error);
-      });
+  //!
+  const fetchUserNameById = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/test_users/${id}`);
+      const data = await response.json();
+      console.log("Fetched user data:", data);
+      return data.name;
+    } catch (error) {
+      console.error("Error fetching the user:", error);
+    }
   };
-
+  
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/feedbacks/${user?.id}`);
+      const data = await response.json();
+      setFeedback(data.feedback);
+      setReceiver(data.receives_uid);
+      setSender(data.gives_uid);
+      setCategory(data.category);
+      
+      const receiverName = await fetchUserNameById(data.receives_uid);
+      console.log("reciever name", receiverName);
+      setReceiver(receiverName); // Now, it's the name
+  
+      // Fetch the sender's name (optional, if sender is not the current user)
+      const senderName = await fetchUserNameById(data.gives_uid);
+      console.log("sender name", senderName);
+      setSender(senderName);
+    } catch (error) {
+      console.error("Error fetching the feedback:", error);
+    }
+  };
+//!  
   useEffect(() => {
     fetchData();
     console.log("hello");
     console.log(feedback);
+    console.log(user);
+    console.log(user?.id);
+    fetchUserNameById();
   }, []);
 
+  // set sender to user?.name
 
   return (
     <main className="main-container">
-        <h1>Feedback</h1>
-        <h1>What is your feedback about {receiver}?</h1>
+      <h1>Feedback</h1>
+      <h1>What is your feedback about {receiver}?</h1>
+      <p>
+        {" "}
+        Hello {sender}, provide feedback about a specific user: {receiver} in
+        category: {category}
+      </p>
 
-        <h1> {feedback}</h1>
+      <h1> {feedback}</h1>
 
-        <h1>User Feedback Form</h1>
-        <p> Hello {sender}, provide feedback about a specific user: {receiver} in category: {category}</p>
+      <h1>User Feedback Form</h1>
+      <p>
+        {" "}
+        Hello {sender}, provide feedback about a specific user: {receiver} in
+        category: {category}
+      </p>
 
-        <form action="submit_feedback.php" method="post">
-            <label htmlFor="user_to_feedback">User to Provide Feedback About: {receiver}</label>
-            <br></br>
+      <form action="submit_feedback.php" method="post">
+        <label htmlFor="user_to_feedback">
+          User to Provide Feedback About: {receiver}
+        </label>
+        <br></br>
 
-            <label htmlFor="feedback">Feedback:</label>
-            <br></br>
+        <label htmlFor="feedback">Feedback:</label>
+        <br></br>
 
-            <textarea id="feedback" name="feedback" rows="4" cols="50" required></textarea>
-            <br></br>
+        <textarea
+          id="feedback"
+          name="feedback"
+          rows="4"
+          cols="50"
+          required
+        ></textarea>
+        <br></br>
 
-            <input type="submit" value="Submit Feedback"/>
-        </form>
+        <input type="submit" value="Submit Feedback" />
+      </form>
     </main>
   );
 }
