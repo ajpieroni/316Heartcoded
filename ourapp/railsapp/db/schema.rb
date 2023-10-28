8e19c0c5ef20025361fd5772031b77f120c3ad26
Jacob Lee <53345177+Jacob-W-Lee@users.noreply.github.com>
2023-10-28 14:26:01 -0400

    axios problem :(

--- ourapp/railsapp/db/schema.rb
+++ ourapp/railsapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_223825) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_26_215725) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_223825) do
     t.integer "gives_uid"
     t.integer "receives_uid"
     t.string "category"
-    t.string "feedback"
+    t.integer "feedback"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- ourapp/railsapp/docker-entrypoint-rails.sh
+++ ourapp/railsapp/docker-entrypoint-rails.sh
@@ -4,6 +4,6 @@ set -e
 
 rake db:create
 rake db:migrate
-# rake db:seed
+rake db:seed
 
 exec "$@"
