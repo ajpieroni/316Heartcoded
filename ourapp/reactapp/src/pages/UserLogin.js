56b10237fd6a9b62eb2fd3c96aaf6c8e4b149cba
Alex <akp42@duke.edu>
2023-11-04 08:20:08 -0400

    gross reorg of landing

--- ourapp/reactapp/src/pages/UserLogin.js
+++ ourapp/reactapp/src/pages/UserLogin.js
@@ -1,128 +1,121 @@
 import React, { useState, useEffect, useContext } from "react";
-import "./UserLogin.css";
 import { useNavigate } from 'react-router-dom';
-
-
-import axios from "axios";
-import { useHistory } from "react-router-dom"; 
-// const history = useHistory();
-import { Link } from "react-router-dom";
 import { UserContext } from "../components/contexts/UserContext";
-
 import ForgotPassword from "./ForgotPassword";
 import CreateProfile from "./CreateProfile.js";
+import "./UserLogin.css";
 
 export default function UserLogin() {
-  
-  // const [question, setQuestion] = useState("UNINIT");
-  const [testUser, setTestUser] = useState("UNINIT");
-
-  const { user, setUser } = useContext(UserContext);
+  // State hooks
+  const [showForgotPassword, setShowForgotPassword] = useState(false);
+  const [showSignIn, setShowSignIn] = useState(true); // Show sign-in by default
+  const [showSignUp, setShowSignUp] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [login, setLogin] = useState(false);
 
-  // const history = useHistory();
+  // Context
+  const { user, setUser } = useContext(UserContext);
+
+  // Navigation hook
   const navigate = useNavigate();
 
+  // Handlers
+  const toggleForgotPassword = () => setShowForgotPassword(prev => !prev);
+  const toggleSignIn = () => {
+    setShowSignIn(!showSignIn);
+    setShowSignUp(false);
+  };
+  const toggleSignUp = () => {
+    setShowSignUp(!showSignUp);
+    setShowSignIn(false);
+  };
+  const handleSignUp = () => {
+    // TODO: SignUp logic
+  };
 
   const initializeUser = () => {
-    console.log("pressed");
+    // User initialization logic
     fetch(`http://localhost:3000/test_users/find_by_username/${username}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
+      .then(response => {
+        if (!response.ok) throw new Error("Network response was not ok");
         return response.json();
       })
-      .then((data) => {
+      .then(data => {
         if (data) {
-          setUser(prevUser => ({
-            ...prevUser, // This ensures we keep the existing fields and only update what's necessary
-            username: data.username, // Or any other relevant field from your data
-            id: data.id,
-            name: data.name,
-            birthday: data.birthday,
-            bio: data.bio,
-            gender: data.gender,
-            join_date: data.join_date,
-            location: data.location,
-            preferences: data.preferences,
-            // ... any other fields you need to update from the response
-          }));
+          setUser(prevUser => ({ ...prevUser, ...data }));
           sessionStorage.setItem("user", JSON.stringify(data));
-       
-          // setTestUser(user)
-          // history.push("/signed-in");
           navigate('/UserSignedIn');
-          console.log("here is data", data);
-          console.log(data.name);
           setLogin(true);
         } else {
-          // Handle non-existing user, if needed
+          // Handle non-existing user logic
         }
       })
-      .catch((error) => {
-        // setUser("Failed to login")
-        console.error("Failed to initialize user:", error);
-      });
+      .catch(error => console.error("Failed to initialize user:", error));
   };
+
+  // Effects
   useEffect(() => {
-    // Check if there's user data in sessionStorage on component initialization
     const storedUser = sessionStorage.getItem("user");
     if (storedUser) {
       setUser(JSON.parse(storedUser));
       setLogin(true);
     }
-  }, []);
+  }, [setUser]);
 
-  // useEffect(() => {
-  //   fetchUserName();
-  //   fetchQuestion();
-  // }, []);
+  // Render helpers
+  const renderSignUpButton = () => (
+    showSignIn && !showForgotPassword && (
+      <button className="sign-up-button" onClick={toggleSignUp}>
+        Sign Up
+      </button>
+    )
+  );
 
+  // Component render
   return (
     <main className="main-container">
-      {/* <img src={Image}/> */}
-      {/* <h1 className="main-title">{question}</h1> */}
       <div className="hero-section">
         <h1 className="hero-title">Welcome to HeartCoded</h1>
         <p className="hero-subtitle">Find your soulmate today!</p>
-        {/* <button className="cta-button">Sign Up Now</button> */}
-        {/* User init */}
         <div className="user-init-container">
-          <input
-            className="user-init-input"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Enter Username"
-          />
-          <input
-            type="password"
-            className="user-init-input"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter Password"
-          />
-          <button className="user-init-button" onClick={initializeUser}>
-            Initialize User
-          </button>
-
-          <ForgotPassword />
-
-          <h2>
-            {login
-              ? `Logged in as: ${user?.name}, Birthday: ${user?.birthday}, ID: ${user?.id}`
-              : "Not Logged In"}
-          </h2>
+          {showSignIn && (
+            <div>
+              <input
+                className="user-init-input"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                placeholder="Enter Username"
+              />
+              <input
+                type="password"
+                className="user-init-input"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                placeholder="Enter Password"
+              />
+              <button className="user-init-button" onClick={initializeUser}>
+                Sign In
+              </button>
+            </div>
+          )}
+          {showSignIn && (
+            <>
+              <button className="forgot-password-toggle" onClick={toggleForgotPassword}>
+                Forgot Password?
+              </button>
+              {showForgotPassword && <ForgotPassword />}
+            </>
+          )}
+          {renderSignUpButton()}
+          {showSignUp && (
+            <>
+              <CreateProfile />
+              <button onClick={toggleSignIn}>Already have an account? Sign In</button>
+            </>
+          )}
+          {showForgotPassword && <ForgotPassword />}
         </div>
-      </div>
-
-      
-
-      <div className="cta-section">
-        <h2 className="cta-title">Ready to find your match?</h2>
-        <button className="cta-button">{testUser}</button>
       </div>
     </main>
   );
