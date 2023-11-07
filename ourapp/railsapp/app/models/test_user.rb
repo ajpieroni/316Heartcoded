db4ff75c3537f9ff1a1aa71a32e6432cafbf7533
Lily Neusaenger <110699229+lneusaenger@users.noreply.github.com>
2023-11-07 16:45:32 -0500

    fixed error where unmatches where not persisting

--- ourapp/railsapp/app/models/test_user.rb
+++ ourapp/railsapp/app/models/test_user.rb
@@ -1,7 +1,7 @@
 class TestUser < ApplicationRecord
     has_many :sent_messages, class_name: 'Message', foreign_key: 'uid_sender_id'
     has_many :received_messages, class_name: 'Message', foreign_key: 'uid_receiver_id'
-    has_secure_password
+    # has_secure_password
     def messages
         Message.where("uid_sender_id = :id OR uid_receiver_id = :id", id: id)
       end
