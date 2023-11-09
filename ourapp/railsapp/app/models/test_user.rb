f3a5871b22643c3fccbc386b565dced367ffad30
Linda Wang <lindawang@Lindas-MacBook-Pro-2.local>
2023-11-09 00:59:48 -0500

    password fixed

--- ourapp/railsapp/app/models/test_user.rb
+++ ourapp/railsapp/app/models/test_user.rb
@@ -1,10 +1,24 @@
+require 'bcrypt'
 class TestUser < ApplicationRecord
     has_many :sent_messages, class_name: 'Message', foreign_key: 'uid_sender_id'
     has_many :received_messages, class_name: 'Message', foreign_key: 'uid_receiver_id'
-    # has_secure_password
+    has_secure_password
+
+    validates :password, presence: true, length: { minimum: 6 }
+    validate :password_complexity
+
     def messages
         Message.where("uid_sender_id = :id OR uid_receiver_id = :id", id: id)
       end
+
+    private
+
+    def password_complexity
+      if password.present? && !password.match(/\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/)
+        errors.add :password, 'must be at least 6 characters long and include both letters and numbers'
+      end
+    end
+
 
     attribute :red_flags, :string, array: true
 
