30b3f5f2253694dae08cc890fb626c345635da67
Alex <akp42@duke.edu>
2023-11-08 22:51:22 -0500

    added usernames

--- ourapp/railsapp/app/models/test_user.rb
+++ ourapp/railsapp/app/models/test_user.rb
@@ -2,9 +2,11 @@ class TestUser < ApplicationRecord
     has_many :sent_messages, class_name: 'Message', foreign_key: 'uid_sender_id'
     has_many :received_messages, class_name: 'Message', foreign_key: 'uid_receiver_id'
     # has_secure_password
+    validates :username, presence: true
+    
     def messages
         Message.where("uid_sender_id = :id OR uid_receiver_id = :id", id: id)
-      end
+    end
 
     attribute :red_flags, :string, array: true
 
