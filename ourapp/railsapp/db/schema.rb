53815742379bd11d9e71a0054d91d4b8546e45e6
Eileen Cai <eileen.cai@duke.edu>
2023-11-19 22:47:15 -0500

    feedback weights column works

--- ourapp/railsapp/db/schema.rb
+++ ourapp/railsapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_09_034440) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_20_023940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_034440) do
     t.integer "gives_uid"
     t.integer "receives_uid"
     t.string "category"
-    t.string "feedback"
+    t.integer "feedback"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -93,7 +93,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_034440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "red_flags", default: [], array: true
-    t.string "password_digest"
     t.string "username"
     t.string "password_digest"
   end
@@ -104,6 +103,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_034440) do
     t.float "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "feedback"
     t.index ["category_id"], name: "index_weights_on_category_id"
     t.index ["test_user_id"], name: "index_weights_on_test_user_id"
   end
