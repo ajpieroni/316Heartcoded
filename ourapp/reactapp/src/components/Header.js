43af31716e0c759db032195f85f415cf5f1796f3
annazhang1029 <anna.zhang@duke.edu>
2023-11-26 01:28:41 -0500

    profile view done

--- ourapp/reactapp/src/components/Header.js
+++ ourapp/reactapp/src/components/Header.js
@@ -3,6 +3,7 @@ import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { UserContext } from './contexts/UserContext';
+import { Link } from 'react-router-dom';
 import './Header.css'; // You can create a Header.css for styling your header
 
 function Header() {
@@ -26,14 +27,35 @@ function Header() {
 
   return (
     <header className="header">
-      <div class = "user-welcome">Welcome, {user?.name}</div>
-
+      <div className="user-welcome">
+        Welcome, {user?.name}
+      </div>
+      <div style={{ marginLeft: '8px' }}>
+      <Link to="/ViewProfile">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="white"
+          style={{ width: '30px', height: '30px' }}
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+        </svg>
+        </Link>
+      </div>
+  
       {/* Logout Button */}
       <button onClick={handleLogout} className="logout-button">
         Logout
       </button>
     </header>
   );
+  
 }
 
 export default Header;
