20b5be5918752ada3fbd5d9b8fa0efccdccae009
Alex <akp42@duke.edu>
2023-11-29 20:02:04 -0500

    workign on message parsing

--- ourapp/reactapp/src/pages/Wingman.js
+++ ourapp/reactapp/src/pages/Wingman.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useContext } from "react";
 import { UserContext } from "../components/contexts/UserContext";
 import axios from "axios";
+import Replicate from "replicate";
+
 // import "./Chat.css";
 import "./Wingman.css"
 
@@ -19,6 +21,32 @@ export default function Chat() {
       setUser(JSON.parse(storedUser));
     }
   }, []);
+  // !API
+
+  const replicate = new Replicate({
+    auth: "r8_Oz4iaoKys1l1aBK9XgNIpZXnoXFvbjr0qdiX3",
+  });
+  
+  async function runModel() {
+    try {
+      const output = await replicate.run(
+        "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
+        {
+          input: {
+            prompt: "Can you write a poem about open source machine learning?"
+          }
+        }
+      );
+      console.log(output);
+    } catch (error) {
+      console.error("Error running the model:", error);
+    }
+  }
+  
+
+  
+
+// !End Llama
 
   const fetchMessages = () => {
     axios.get(`http://localhost:3000/messages/${user?.id}/${botId}`)
@@ -32,18 +60,33 @@ export default function Chat() {
   const sendMessageToBot = async (text) => {
     try {
       const data = {
-        inputs: {
-          past_user_inputs: messages.map(msg => msg.message),
-          generated_responses: messages.map(msg => msg.isBot ? msg.message : null).filter(msg => msg),
-          text: text
-        }
+        inputs: text,
       };
-      const response = await fetch("https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium", {
-        headers: { Authorization: "Bearer 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" }, 
-        method: "POST",
-        body: JSON.stringify(data),
-      });
-      const result = await response.json();
+      console.log('Sending to bot:', data);
+  
+      const response = await fetch(
+        "https://api-inference.huggingface.co/models/meta-llama/Llama-2-70b-chat-hf",
+        {
+          headers: { 'Content-Type': 'application/json', Authorization: "Bearer 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" }, // Replace with your actual token
+          method: "POST",
+          body: JSON.stringify(data),
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const tempresult = await response.json();
+      const result = tempresult[0].generated_text;
+      console.log(result);
+      const regex = /"\s*([^"]*)"/;
+      const match = result.match(regex);
+
+      if (match && match[1]) {
+          console.log(match[1]);
+      } else {
+          console.log("No match found");
+      }
+      // const parsedresult = 
       return result;
     } catch (error) {
       console.error('Error sending message to the bot:', error);
@@ -69,7 +112,7 @@ export default function Chat() {
       const botMessage = {
         id: messages.length + 2, // simplistic way to generate a unique ID
         uid_sender_id: "AI_BOT_ID",
-        message: botResponse.generated_text, // You might need to adjust this depending on the shape of your response
+        message: botResponse,
         timestamp: Date.now(),
         isBot: true
       };
@@ -102,7 +145,7 @@ export default function Chat() {
           })}
         </div>
         {isSending && (<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>)}
-
+        <button onClick = {runModel}>RUN MODEL</button>
         <div className="message-input-container">
           <input
             type="text"
