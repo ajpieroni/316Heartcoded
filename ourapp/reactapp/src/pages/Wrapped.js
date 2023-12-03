ca0e2f52f4c79a45ba7e748579d20d867b57accd
Alex <akp42@duke.edu>
2023-12-03 17:34:17 -0500

    started wrapp

--- ourapp/reactapp/src/pages/Wrapped.js
+++ ourapp/reactapp/src/pages/Wrapped.js
@@ -14,6 +14,33 @@ export default function Wrapped() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [login, setLogin] = useState(false);
+  const [questionsAnswered, setQuestionsAnswered] = useState("")
+  const [mostValuedCategory, setMostValuedCategory] = useState("");
+
+  const fetchQuestions = () => {
+    axios
+      .get(` http://localhost:3000/answered_questions_count/${user?.id}`)
+      .then((response) => {
+        console.log(response.data)
+        setQuestionsAnswered(response.data.answered_question_count)
+      })
+      .catch((error) => {
+        console.error("Error fetching the messages:", error);
+      });
+  };
+  const fetchMostValuedCategory = () => {
+    axios
+      .get(`http://localhost:3000/user_most_valued_category/${user?.id}`)
+      .then((response) => {
+        console.log(response.data)
+        setMostValuedCategory(response.data.answered_question_count)
+      })
+      .catch((error) => {
+        console.error("Error fetching the messages:", error);
+      });
+  };
+  
+  
 
 
   useEffect(() => {
@@ -22,12 +49,25 @@ export default function Wrapped() {
     if (storedUser) {
       setUser(JSON.parse(storedUser));
       setLogin(true);
+      if(user.id){
+        fetchQuestions();
+        fetchMostValuedCategory();
+      }
     }
+    
   }, []);
+
+  console.log(user?.id)
+
+  
 
 
   return (
     <main className="main-container">
+        <h1>{user?.name}'s Wrapped</h1>
+        <h1>Questions Answered: {questionsAnswered}</h1>
+        {/* <button onClick={fetchQuestions}>How many questions answered?</button> */}
+
      
     </main>
   );
