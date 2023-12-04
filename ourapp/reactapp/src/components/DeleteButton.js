f2f42d3997a2c9742888b793c5cc16e099e80f5b
Alex <akp42@duke.edu>
2023-12-04 01:38:51 -0500

    routing delete button, funciton not working

---
+++ ourapp/reactapp/src/components/DeleteButton.js
@@ -0,0 +1,53 @@
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../components/contexts/UserContext";
+
+const DeleteProfile = () => {
+  const { user, setUser } = useContext(UserContext);
+  const [confirmation, setConfirmation] = useState("");
+  const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    if (confirmation === "DELETE") {
+      try {
+        if (!user || !user.id) {
+          console.error("User ID is not available.");
+          return;
+        }
+
+        const response = await axios.delete(
+          `http://localhost:3000/test_users/${user.id}`
+        );
+
+        if (response.status === 200) {
+          console.log("User deleted successfully");
+          // Here you should also clear the user context and any stored sessions
+          setUser(null);
+          sessionStorage.removeItem("user");
+          localStorage.removeItem("username");
+          navigate("/"); // Navigate to the homepage or login page
+        } else {
+          console.error("Failed to delete user");
+        }
+      } catch (error) {
+        console.error("Error deleting user:", error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Delete Your Profile</h1>
+      <input
+        type="text"
+        placeholder="Type DELETE to confirm"
+        value={confirmation}
+        onChange={(e) => setConfirmation(e.target.value)}
+      />
+      <button onClick={handleDelete}>Confirm Deletion</button>
+    </div>
+  );
+};
+
+export default DeleteProfile;
