0d9b12c4c19662615c171cf086beec2ee5465af5
Alex <akp42@duke.edu>
2023-12-03 22:08:41 -0500

    most valued categories working

--- ourapp/reactapp/src/pages/Wrapped.js
+++ ourapp/reactapp/src/pages/Wrapped.js
@@ -3,26 +3,25 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import { UserContext } from "../components/contexts/UserContext";
 // import bcrypt from 'bcryptjs';
-
-
+import Header from "../components/Header";
 
 export default function Wrapped() {
   const [question, setQuestion] = useState("UNINIT");
   const [testUser, setTestUser] = useState("UNINIT");
   const [error, setError] = useState(null);
-  const {user, setUser} = useContext(UserContext);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const { user, setUser } = useContext(UserContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [login, setLogin] = useState(false);
-  const [questionsAnswered, setQuestionsAnswered] = useState("")
-  const [mostValuedCategory, setMostValuedCategory] = useState("");
+  const [questionsAnswered, setQuestionsAnswered] = useState("");
+  const [mostValuedCategory, setMostValuedCategory] = useState([]);
 
   const fetchQuestions = () => {
     axios
       .get(` http://localhost:3000/answered_questions_count/${user?.id}`)
       .then((response) => {
-        console.log(response.data)
-        setQuestionsAnswered(response.data.answered_question_count)
+        console.log(response.data);
+        setQuestionsAnswered(response.data.answered_question_count);
       })
       .catch((error) => {
         console.error("Error fetching the messages:", error);
@@ -32,44 +31,51 @@ export default function Wrapped() {
     axios
       .get(`http://localhost:3000/user_most_valued_category/${user?.id}`)
       .then((response) => {
-        console.log(response.data)
-        setMostValuedCategory(response.data.answered_question_count)
+        console.log("here category", response.data);
+        console.log("here parsed", response.data.category_descriptors);
+
+        setMostValuedCategory(response.data.category_descriptors);
+        console.log("here set value", mostValuedCategory);
       })
       .catch((error) => {
         console.error("Error fetching the messages:", error);
       });
   };
-  
-  
-
 
   useEffect(() => {
-    // Check if there's user data in sessionStorage on component initialization
+    console.log("New most valued category:", mostValuedCategory);
+  }, [mostValuedCategory]);
+
+  useEffect(() => {
     const storedUser = sessionStorage.getItem("user");
     if (storedUser) {
       setUser(JSON.parse(storedUser));
       setLogin(true);
-      if(user.id){
-        fetchQuestions();
-        fetchMostValuedCategory();
-      }
     }
-    
   }, []);
 
-  console.log(user?.id)
-
-  
-
+  useEffect(() => {
+    if (user && user.id) {
+      fetchQuestions();
+      fetchMostValuedCategory();
+    }
+  }, [user]);
 
   return (
-    <main className="main-container">
+    <div>
+      <Header />
+
+      <main className="main-container">
         <h1>{user?.name}'s Wrapped</h1>
         <h1>Questions Answered: {questionsAnswered}</h1>
-        {/* <button onClick={fetchQuestions}>How many questions answered?</button> */}
-
-     
-    </main>
+        <h2>Most Valued Categories:</h2>
+        <ul>
+          {/* Directly map over mostValuedCategory */}
+          {mostValuedCategory.map((descriptor, index) => (
+            <li key={index}>{descriptor}</li>
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 }
-
