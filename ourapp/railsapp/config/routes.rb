5363b43cfa0f5766bcf161765a63d74b26615c13
Alex <akp42@duke.edu>
2023-12-03 22:58:39 -0500

    top messaged

--- ourapp/railsapp/config/routes.rb
+++ ourapp/railsapp/config/routes.rb
@@ -45,6 +45,7 @@ Rails.application.routes.draw do
   get 'num_unmatches/:id', to: 'matched_withs#num_unmatches'
   get 'messages/num_messages_sent/:uid_sender_id', to: 'messages#num_messages_sent'
   get 'messages/num_messages_gotten/:uid_receiver_id', to: 'messages#num_messages_gotten'
+  get 'messages/top_three_messaged_users/:uid_sender_id', to: 'messages#top_three_messaged_users'
 
   # Uncomment the following if you're using these APIs and ensure their correct placement within the namespace
   # namespace :api do
