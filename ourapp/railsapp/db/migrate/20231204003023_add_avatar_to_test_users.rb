5c9d2e01e5c22d2aea35c7300529c7ed583a85ff
Alex <akp42@duke.edu>
2023-12-05 04:00:02 -0500

    remove unused migrations

--- ourapp/railsapp/db/migrate/20231204003023_add_avatar_to_test_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAvatarToTestUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :test_users, :avatar, :attachment
-  end
-end
--- ourapp/railsapp/db/migrate/20231204003553_add_avatar_to_test_users_again.rb
+++
@@ -1,4 +0,0 @@
-class AddAvatarToTestUsersAgain < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
